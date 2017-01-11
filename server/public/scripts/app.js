551dbb0ab7074e04b79b398b6810784a4661163f
NakajimaShinya <nakajima.shinya11@gmail.com>
2017-01-11 22:27:10 +0900

    heroku

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -21,7 +21,7 @@ angular
 
   .config(function ($routeProvider, $httpProvider, RestangularProvider) {
     // RestangularProvider.setBaseUrl('http://localhost:3000')
-    RestangularProvider.setBaseUrl('https://travel-ex.herokuapp.com:433/')
+    RestangularProvider.setBaseUrl('https://travel-ex.herokuapp.com/')
     $routeProvider
       .when('/', {
         redirectTo: '/application/new'
